e237637d9c9fcbdf7bd3d3901fe3c18e40ab5a5b
jdvachhani95 <jaydeepvachhani22@gmail.com>
2021-01-24 18:24:28 -0500

    added helmet

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,13 @@ import { ApolloServer } from "apollo-server-express";
 import { createServer } from "http";
 import compression from "compression";
 import cors from "cors";
+import helmet from "helmet";
 import { schema } from "./schema";
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 app.use("*", cors());
+app.use(helmet());
 app.use(compression());
 const server = new ApolloServer({
   schema,
